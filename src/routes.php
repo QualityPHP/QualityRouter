f905c7c216230d580c61d9b37d3d8adc213403c3
yak0d3 <contact.raedyak@gmail.com>
2019-01-12 00:39:09 +0100

    Add description for the 'CONFSET' method usage

--- src/routes.php
+++ src/routes.php
@@ -2,12 +2,11 @@
 
 namespace QualityPHP\QualityRouter;
 
-function pointTo(string $filename)
-{
-    include $filename;
-}
+// QRouter::CONFSET('AUTO', true); # This will make the router ignnore all routes below
+                                   # and respond with file contents from the app dir (provided in the config.php file)
+                                   # e.g. Request uri = /FILENAME, then the response will be /APP_DIR/FILENAME.php
 
-QRouter::POST('/', function() {
+QRouter::POST('/', function () {
     echo "Test";
 });
 
